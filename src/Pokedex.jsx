3f4349cad7f5b0b8fc17fb5ed278fe628b1acad3
ehsan saraee <ehsansaraee6570@yahoo.com>
2021-06-26 02:14:31 +0430

    Grid layout with data

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+   Card,
+   Grid,
+   CardContent,
+   CardMedia,
+   AppBar,
+   Toolbar,
+   CircularProgress,
+   Typography,
+   capitalize,
+} from "@material-ui/core";
+import mockData from "./mockData";
 
-const Pokedex = () => {
-    return (
-        <div>
-            This is the pokedex page
-        </div>
-    )
-}
+const useStyles = makeStyles({
+   pokedexContainer: {
+      padding: "20px 50px 0 50px",
+   },
+   cardMedia: {
+      margin: "auto",
+   },
+   cardContent: {
+      textAlign: "center",
+   },
+});
 
-export default Pokedex
+const Pokedex = ({ history }) => {
+   const { pokedexContainer, cardMedia, cardContent } = useStyles();
+   const [pokemonData, setPokemonDat] = useState(mockData);
+
+   const getPokemonCard = (pokemonId) => {
+      const { id, name } = pokemonData[`${pokemonId}`];
+      const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+      return (
+         <Grid item xs={4} key={pokemonId}>
+            <Card onClick={() => history.push(`/${pokemonId}`)}>
+               <CardMedia
+                  className={cardMedia}
+                  image={sprite}
+                  style={{ width: "130px", height: "130px" }}
+               />
+               <CardContent className={cardContent}>
+                  <Typography>{`${id}. ${capitalize(name)}`}</Typography>
+               </CardContent>
+            </Card>
+         </Grid>
+      );
+   };
+   return (
+      <>
+         <AppBar position="static">
+            <Toolbar />
+         </AppBar>
+         {pokemonData ? (
+            <Grid container spacing={2} className={pokedexContainer}>
+               {Object.keys(pokemonData).map((pokemonId) =>
+                  getPokemonCard(pokemonId)
+               )}
+            </Grid>
+         ) : (
+            <CircularProgress />
+         )}
+      </>
+   );
+};
+
+export default Pokedex;
